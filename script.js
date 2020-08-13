91ea6c7e0d775538eb61224b689ef5f132a705f4
Sean Birmingham <seannbirmingham98@gmail.com>
2020-08-13 14:34:04 -0400

    functionality to show only millionaires

--- script.js
+++ script.js
@@ -42,6 +42,13 @@ function sortByRichest() {
   updateDOM();
 }
 
+// Show only millionaires
+function showMillionaires() {
+  data = data.filter((person) => person.money >= 1000000);
+
+  updateDOM();
+}
+
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj);
@@ -74,3 +81,4 @@ function formatMoney(number) {
 addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionaires);
